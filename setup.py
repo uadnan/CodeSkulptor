bbf99853a083646e84e16c55158ae980903ce81e
Adnan Umer <u.adnan@outlook.com>
2018-09-16 23:04:53 +0500

    Post install instructions

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 import os
+import shutil
+import zipfile
+
 from setuptools import find_packages, setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
 
 
 def read(file_name):
@@ -9,9 +14,62 @@ def read(file_name):
 
 from codeskulptor import __version__
 
+GITHUB_RELEASE = "6e60cf9dba27dea3f405"
 EXCLUDE_FROM_PACKAGES = [
     'codeskulptor.bin',
 ]
+
+
+def download_file(url):
+    import requests
+    import tempfile
+
+    directory = tempfile.mkdtemp()
+    local_filename = os.path.join(directory, "www.zip")
+
+    r = requests.get(url, stream=True)
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+
+    return local_filename
+
+
+def clean_directory(directory):
+    if os.path.exists(directory):
+        shutil.rmtree(directory)
+
+
+class PostInstallCommand(install):
+    def run(self):
+        from codeskulptor import WWW_ROOT
+
+        clean_directory(os.path.join(WWW_ROOT, "py2"))
+        clean_directory(os.path.join(WWW_ROOT, "py3"))
+
+        local_zip_path = None
+        try:
+            local_zip_path = download_file(
+                "https://github.com/uadnan/CodeSkulptor/releases/download/{}/www.zip".format(GITHUB_RELEASE)
+            )
+
+            with zipfile.ZipFile(local_zip_path, "r") as f:
+                f.extractall(WWW_ROOT)
+        finally:
+            if os.path.exists(local_zip_path):
+                os.remove(local_zip_path)
+
+        install.run(self)
+
+
+class PostDevelopCommand(develop):
+    def run(self):
+        from codeskulptor.interface import run_grabber
+
+        run_grabber()
+        develop.run(self)
+
 
 setup(
     name='CodeSkulptor',
@@ -43,5 +101,9 @@ setup(
         'License :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python'
-    ]
+    ],
+    cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+    },
 )
