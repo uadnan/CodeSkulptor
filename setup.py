6079c6e7bf932f899a5bef4722963d30044240bb
Adnan Umer <u.adnan@outlook.com>
2019-07-23 17:01:56 +0500

    Warn on non-compatible python version

--- setup.py
+++ setup.py
@@ -16,6 +16,18 @@ def read(file_name):
         return f.read()
 
 
+CURRENT_PYTHON = sys.version_info[:2]
+REQUIRED_PYTHON = (3, 5)
+
+if CURRENT_PYTHON < REQUIRED_PYTHON:
+    sys.stderr.write(
+        "This version of Codeskulptor requires Python {}.{}, but you're trying to install it on Python {}.{}. "
+        "Please install Python {}.{} or higher.".format(
+            *(REQUIRED_PYTHON + CURRENT_PYTHON + REQUIRED_PYTHON)
+        )
+    )
+    sys.exit(1)
+
 GITHUB_RELEASE = "2018.09.16"
 REQUESTS_LIB = "requests==2.22.0"
 EXCLUDE_FROM_PACKAGES = [
